8841cc02c839500c802443d2da4ca22fdabb148b
tnydilya <49895917+tnydilya@users.noreply.github.com>
2019-04-23 16:55:59 +0600

    Add files via upload

---
+++ build/Pods.build/Debug-iphonesimulator/EasyPeasy.build/DerivedSources/EasyPeasy_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char EasyPeasyVersionString[];
+ extern const double EasyPeasyVersionNumber;
+
+ const unsigned char EasyPeasyVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:EasyPeasy  PROJECT:Pods-1" "\n";
+ const double EasyPeasyVersionNumber __attribute__ ((used)) = (double)1.;
